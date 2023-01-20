7e7f8c1d5eb551fc1259f275e5381adb0e1877b4
Azra,Korkmaz <Azra.Korkmaz@accenture.korkmaz@accenture.com>
2023-01-20 10:39:17 +0100

    tier, personal

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Tiere Loewe1 = new Tiere("Säugetier", 1, "Fleischfresser", 140.0);
+        Tiere Loewe1 = new Tiere("Löwe", "Rolle", "Säugetier", 1, "Fleischfresser", 140.0);
 
-        Tiere Schlange1 = new Tiere("Reptiel", 1, "Fleischfresser", 5);
+        Tiere Schlange1 = new Tiere("Schlange", "Ronja","Reptiel", 1, "Fleischfresser", 5);
 
-        Tiere Ziege1 = new Tiere("Säugetier", 1, "Pflanzenfresser", 60);
+        Tiere Ziege1 = new Tiere("Ziehge", "Sandra", "Säugetier", 1, "Pflanzenfresser", 60);
 
 
 
