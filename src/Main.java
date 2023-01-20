1492551912a0a33e879ab050777137397dae9ac1
Anastasija.Semtsiv <anastasija.semtsiv@accenture.de>
2023-01-20 10:03:54 +0100

    Inital Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        Tiere Loewe1 = new Tiere("Säugetier", 1, "Fleischfresser", 140.0);
+
+        Tiere Schlange1 = new Tiere("Reptiel", 1, "Fleischfresser", 5);
+
+        Tiere Ziege1 = new Tiere("Säugetier", 1, "Pflanzenfresser", 60);
+
+
+
+
+
     }
 }
--- src/Tiere.java
+++ src/Tiere.java
@@ -1,2 +1,58 @@
 public class Tiere {
+    private String Typ;
+
+    private String Name;
+    private String Art;
+    private int Anzahl;
+    private String Nahrung;
+    private double Gewicht;
+
+
+    private void setAttributes(String Art, int Anzahl, String Nahrung, double Gewicht) {
+        this.Art = Art;
+        this.Anzahl = Anzahl;
+        this.Nahrung = Nahrung;
+        this.Gewicht = Gewicht;
+    }
+
+    public void displayAttributes() {
+        System.out.println("\nArt: " + Art);
+        System.out.println("Anzahl: " + Anzahl);
+        System.out.println("Nahrung?: " + Nahrung);
+        System.out.println("Gewicht?: " + Gewicht);
+    }
+
+
+    public void setArt(String Art) {
+        Art = Art;
+    }
+
+    public String getArt() {
+        return Art;
+    }
+
+    public double getAnzahl() {
+        return Anzahl;
+    }
+
+    public void setNahrung(String Nahrung) {
+        Nahrung = Nahrung;
+    }
+
+    public String getNahrung() {
+        return Nahrung;
+    }
+
+    public void setGewicht(double Gewicht) {
+        Gewicht = Gewicht;
+    }
+
+    public double getGewicht() {
+        return Gewicht;
+    }
+
+
+    public Tiere(String Art, int Anzahl, String Nahrung, double Gewicht) {
+        setAttributes(Art, Anzahl, Nahrung, Gewicht);
+    }
 }
